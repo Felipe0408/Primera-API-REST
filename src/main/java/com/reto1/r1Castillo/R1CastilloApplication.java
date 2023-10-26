a81641be2251abd622259c2d7060b486346b34e6
Felipe0408 <105187424+Felipe0408@users.noreply.github.com>
2023-10-26 00:11:01 -0500

    Se creó proyecto
    Inicio del proyecto de API Rest

---
+++ src/main/java/com/reto1/r1Castillo/R1CastilloApplication.java
@@ -0,0 +1,13 @@
+package com.reto1.r1Castillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R1CastilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R1CastilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto1/r1Castillo/R1CastilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto1.r1Castillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R1CastilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
